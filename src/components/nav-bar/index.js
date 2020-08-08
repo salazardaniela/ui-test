afe2513bbffa8d0c3e8295b257a22a0fde97afdc
Daniela Salazar <danii.salazar00@gmail.com>
2020-08-08 17:13:52 -0500

    Finish nav bar component

--- src/components/nav-bar/index.js
+++ src/components/nav-bar/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './navbar.scss';
 
 const NavBar = () => (
-  <div className="navBar">
+  <section className="navBar">
     <div className="main-wrapper">
       <a className="site" href="/">Rule of Thumb.</a>
-      <nav role="navigation" aria-label="Main">
-        <a href="/page">Past Trials</a>
-        <a href="/page">How It Works</a>
-        <a href="/page">Log In / Sign Up</a>
-      </nav>
-      <button className="button-icon">Search</button>
+      <div className="container-actions">
+        <nav role="navigation" aria-label="Main">
+          <a href="/page">Past Trials</a>
+          <a href="/page">How It Works</a>
+          <a href="/page">Log In / Sign Up</a>
+        </nav>
+        <button className="button-icon">Search</button>
+      </div>
     </div>
-  </div>
+  </section>
 );
 
 export default NavBar;
